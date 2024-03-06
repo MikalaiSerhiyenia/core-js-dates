73df7f2af2c474343c62d843c4cb2b39415aa128
mikalaiserhiyenia <mikalai.serhiyenia@gmail.com>
2024-03-06 14:51:25 +0300

    feat: 2 passing, 12 pending (Wed, Mar 06, 2024 02:51:25 PM)

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,12 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const time = new Date(date);
+  const hours = `0${time.getHours()}`.slice(-2);
+  const minutes = `0${time.getMinutes()}`.slice(-2);
+  const seconds = `0${time.getSeconds()}`.slice(-2);
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
