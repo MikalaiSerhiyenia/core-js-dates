ee15357bbcefce28a9cba5c3e9aa28df2254282d
mikalaiserhiyenia <mikalai.serhiyenia@gmail.com>
2024-03-07 16:26:22 +0300

    feat: 8 passing, 6 pending (Thu, Mar 07, 2024 04:26:22 PM)

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -173,8 +173,21 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const dateStart = new Date(year, month - 1, 1);
+  const dateEnd = new Date(year, month, 0);
+  let count = 0;
+  for (
+    let date = dateStart;
+    date <= dateEnd;
+    date.setDate(date.getDate() + 1)
+  ) {
+    const dayOfWeek = date.getDay();
+    if (dayOfWeek === 0 || dayOfWeek === 6) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /**
